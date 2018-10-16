4a5810b0dc46c9ebf4b17ed8fa721a191336b7a7
Reid Campolong <reidcampolong@gmail.com>
2018-10-15 22:17:25 -0400

    Finished layout and design.

--- app/src/main/java/cadb/gov/zipvote/djd6621.java
+++ app/src/main/java/cadb/gov/zipvote/djd6621.java
@@ -17,28 +17,28 @@ public class djd6621 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_djd6621);
 
-        ImageView candidate1 = findViewById(R.id.imageRed1);
-        ImageView candidate2 = findViewById(R.id.imageRep2);
+        ImageView candidate1 = findViewById(R.id.imageDem1);
+        ImageView candidate2 = findViewById(R.id.imageDem2);
         ImageView candidate3 = findViewById(R.id.imageRep3);
 
         candidate1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                openNextScreen("Republican1", "Republican", "This is an example of Candidate 1's view points on different issues.");
+                openNextScreen("Candidate1", "Republican", "This is an example of Candidate 1's view points on different issues.");
             }
         });
 
         candidate2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                openNextScreen("Republican2", "Republican", "This is an example of Candidate 2's view points on different issues.");
+                openNextScreen("Candidate2", "Republican", "This is an example of Candidate 2's view points on different issues.");
             }
         });
 
         candidate3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                openNextScreen("Republican3", "Republican", "This is an example of Candidate 3's view points on different issues.");
+                openNextScreen("Candidate3", "Republican", "This is an example of Candidate 3's view points on different issues.");
             }
         });
     }
--- app/src/main/java/cadb/gov/zipvote/jpa.java
+++ app/src/main/java/cadb/gov/zipvote/jpa.java
@@ -1,13 +1,43 @@
 package cadb.gov.zipvote;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class jpa extends AppCompatActivity {
 
+    private TextView issueDescription;
+    private TextView greenText;
+    private TextView redText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jpa);
+
+        issueDescription = findViewById(R.id.issueDescription);
+        greenText = findViewById(R.id.issueGreenList);
+        redText = findViewById(R.id.issueRedText);
+
+        String desc = getIntent().getStringExtra("DESC");
+        String green = getIntent().getStringExtra("PRO");
+        String red = getIntent().getStringExtra("RED");
+
+        issueDescription.setText(desc);
+        greenText.setText(green);
+        redText.setText(red);
+
+        issueDescription.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+
+    private void openActivity(){
+        Intent i = new Intent(this, John_Candidates_by_color.class);
+        startActivity(i);
     }
 }
